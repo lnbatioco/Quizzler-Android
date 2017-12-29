d9fd7e9b9ae902231ea7ae28cd12e3018f828136
lnbatioco <LoryleenBatioco@gmail.com>
2017-12-29 13:51:24 -0800

    link true and false buttons

--- app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/quizzler/MainActivity.java
@@ -2,13 +2,17 @@ package com.londonappbrewery.quizzler;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
 
     // TODO: Declare constants here
 
 
-    // TODO: Declare member variables here:
+    // TODO: Declare member variables here: (accessible everywhere!)
+        // create a variable that will hold our button so we can access it anywhere
+        Button mTrueButton; // refer to onCreate
+        Button mFalseButton;
 
 
     // TODO: Uncomment to create question bank
@@ -33,6 +37,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mTrueButton = (Button) findViewById(R.id.true_button); // convert a view to a button
+        mFalseButton = (Button) findViewById(R.id.false_button);
 
 
     }
